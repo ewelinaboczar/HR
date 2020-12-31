c096c64f0504c91b48eb2c7a8560385ca00292fe
ewelinaboczar <ewelina.boczar@student.pk.edu.pl>
2020-12-31 13:44:44 +0100

    ready

---
+++ src/HRMS.cpp
@@ -0,0 +1,206 @@
+#include <employee.hpp>
+#include <HRMS.hpp>
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+class WrongOpenFile : public exception
+{
+    virtual const char *what() const throw()
+    {
+        return "Exeption2: Blad otwarcia pliku";
+    }
+};
+
+/* Czytanie z pliku gotowych 10 pracowników */
+void hrms::read_from_file_workers()
+{
+    fstream plik1;
+    plik1.open("Workers.txt");
+    if(plik1.good())
+    {
+        while(!plik1.eof())
+        {
+            string name, id, surname, depertment, position;
+            getline(plik1, id);
+            getline(plik1, name);
+            getline(plik1, surname);
+            getline(plik1, depertment);
+            getline(plik1, position);
+            workers.push_back(employee{id, name, surname, depertment, position});
+        }
+    }
+    else
+    {
+        throw WrongOpenFile();
+    }
+    plik1.close();
+}
+
+/* Czytanie z pliku pensji */
+void hrms::read_from_file_salary()
+{
+    fstream plik2;
+    plik2.open("salary.txt");
+    if(plik2.good())
+    {
+        while(!plik2.eof())
+        {
+            string ID;
+            double sal;
+            plik2>>ID;
+            plik2>>sal;
+            salary.push_back(make_pair(ID,sal));
+        }
+    }
+    else
+    {
+        throw WrongOpenFile();
+    }
+    plik2.close();
+}
+
+/* Dodawanie nowego pracownika na koniec pliku */
+void hrms::add_new_worker(employee worker) 
+{
+    workers.push_back(worker);
+    fstream plik3;
+    fstream plik5;
+    plik3.open("Workers.txt", std::ios::app);
+    plik5.open("salary.txt", std::ios::app);
+    if(plik3.good())
+    {
+        plik3 << endl;
+        plik3 << worker.id << endl;
+        plik3 << worker.name << endl;
+        plik3 << worker.surname << endl;
+        plik3 << worker.departmentId << endl;
+        plik3 << worker.position;
+    }
+    else throw WrongOpenFile();
+    if(plik5.good())
+    {
+        plik5 << endl;
+        plik5 << worker.id;
+    }
+    else throw WrongOpenFile();
+    plik5.close();
+}
+
+/* Drukowanie pracowników z konteneru (workers)*/
+void hrms::print_workers()
+{
+    int number =1;
+    cout <<left<<setw(6)<< "Lp"<<setw(10)<< "ID"<<setw(15)<<"Imie"<<setw(15)<<"Nazwisko"<<setw(15)<<"Department"<<setw(15)<<"Position"<<endl;
+    cout<<"-----------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<workers.size();i++)
+    {
+        workers[i].print(number);
+        number++;
+    }
+}
+
+/* Tworzenie konteneru (map_workers) */
+void hrms::new_map()
+{
+    for(int i=0;i<workers.size();i++)
+    {
+        map_workers[workers[i].departmentId].push_back(workers[i].id);
+    }
+}
+
+/* Drukowanie konteneru (map_workers) */
+void hrms::print_map()
+{
+    int how_many = map_workers.size();
+    map<string, vector <string>>::iterator it = map_workers.begin();
+    cout<<setw(10)<<"DEPARTMENT"<<setw(10)<<"ID"<<endl;
+    cout<<"--------------------------------"<<endl;
+    for(int i=0; i<how_many; i++)
+    {
+        string depart = it->first;
+        for(int j=0; j <map_workers[it->first].size(); j++)
+        {
+            cout << setw(10)<<depart <<setw(10)<< map_workers[depart][j] << endl;
+        }
+        *it++;
+    }
+}
+
+/* Dodawanie i drukowanie pracowników wraz z wynagrodzeniem */
+void hrms::print_salary()
+{
+    int size_of_vector=salary.size();
+    cout<<size_of_vector<<endl;
+    cout <<left<<setw(6)<< "ID"<<setw(15)<<"Imie"<<setw(15)<<"Nazwisko"<<setw(15)<<"Department"<<setw(15)<<"Position"<<setw(15)<<"Pensja" <<endl;
+    cout<<"---------------------------------------------------------------------------------------"<<endl;
+    for(int i=0;i<size_of_vector;i++)
+    {
+        string actual_id=salary[i].first;
+        int k;
+        for(k=0; workers[k].id!=actual_id; ++k);
+        
+        cout <<left<<setw(6)<<actual_id<<setw(15)<<workers[k].name<<setw(15)<<workers[k].surname<<setw(15)<<workers[k].departmentId<<setw(15)<<workers[k].position<<setw(15)<<salary[i].second<<endl;
+        cout<<"_______________________________________________________________________________________"<<endl;
+    }
+}
+
+/* Sortowanie pracowników od najniższej pensji do najwyższej */
+void hrms::print_sorted_salary()
+{
+    sort(salary.begin(), salary.end(), [](auto &left, auto &right) { return left.second < right.second; });
+    print_salary();
+}
+
+/* Dodawanie nowego pracownika do kontenerów */
+void hrms::add(employee employee, std::string departmentid, double sal)
+{
+    fstream plik5;
+    plik5.open("salary.txt", std::ios::app);
+
+    workers.push_back(employee);
+    map_workers[workers[workers.size() - 1].departmentId].push_back(workers[workers.size() - 1].id);
+    salary.push_back(make_pair(employee.id,sal));
+
+    if(plik5.good())
+    {
+        plik5 << endl;
+        plik5 << sal;
+    }
+    else throw WrongOpenFile();
+    plik5.close();
+}
+
+/* Drukowanie pracownikow z danego departamentu */
+void hrms::print_depart(string departmentid)
+{
+    int how_many = map_workers[departmentid].size();
+    cout<<"Ilosc pracownikow w: "<<departmentid<<" = "<<how_many<<endl;
+    cout<<setw(10)<<"DEPARTMENT"<<setw(10)<<"ID"<<endl;
+    cout<<"___________________________________________"<<endl;
+    for(int i=0; i<how_many; i++)
+    {
+        std::cout <<setw(10)<< departmentid <<setw(10)<< map_workers[departmentid][i] << '\n';
+    }
+}
+
+/* Zmiana wynagrodzenia */
+void hrms::changeSalary(string enmployeeId, double sal)
+{
+    int size_of_vector=salary.size();
+    for(int i=0;i<size_of_vector;i++)
+    {
+        string actualid=salary[i].first;
+        if(actualid==enmployeeId)
+        {
+            salary[i].second=sal;
+        }
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <employee.hpp>
+#include <HRMS.hpp>
+
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+using namespace std;
+ 
+int main()
+{
+    system("cls");
+    bool p=true;
+    hrms nowy;
+    nowy.read_from_file_workers();
+    nowy.read_from_file_salary();
+    nowy.new_map();
+    while(p)
+    {
+        try
+        {
+        cout<<"Wybierz:"<<endl;
+        cout<<"0. Koniec"<<endl;
+        cout<<"1. Drukowanie wszystkich pracownikow (test kontenera workers)"<<endl;
+        cout<<"2. Drukowanie mapy pomiedzy pracownikami a ID departamentu (test kontenera map_workers)"<<endl;
+        cout<<"3. Drukowanie pracownikow znajdujacych sie w zadanym departamencie (test metody print_depart)"<<endl;
+        cout<<"4. Drukowanie zarobkow pracownikow wraz z ich ID (test metody print_salary)"<<endl;
+        cout<<"5. Drukowanie posortowanych zarobkow wraz z danymi pracownikow (test metody print_sorted_salary)"<<endl;
+        cout<<"6. Zmiana pensji wybranego pracownika (test metody changeSalary)"<<endl;
+        cout<<"7. Dodawanie nowego pracownika wraz z jego wynagrodzeniem (test metody add)"<<endl;
+        int ch;
+        cin>>ch;
+        switch(ch)
+        {
+            case 0:
+            {
+                p=false;
+                break;
+            }
+            case 1:
+            {
+                cout << "Drukowanie wszystkich pracownikow" <<endl;
+                nowy.print_workers();
+                break;
+            }
+            case 2:
+            {
+                cout << "Drukowanie mapy pomiedzy pracownikami a ID departamentu"<<endl;
+                nowy.print_map();
+                break;
+            }
+            case 3:
+            {
+                cout << "Podaj nazwe departamentu z ktorego pracownikow chcesz znalezc:" << endl;
+                string dep;
+                cin >> dep;
+                nowy.print_depart(dep);
+                break;
+            }
+            case 4:
+            {
+                cout << "Drukowanie zarobkow:" << endl;
+                nowy.print_salary();
+                break;
+            }
+            case 5:
+            {
+                cout << "Drukowanie posortowanych zarobkow pracownikow" << endl;
+                nowy.print_sorted_salary();
+                break;
+            }
+            case 6:
+            {
+                string new_id;
+                double new_salary;
+                cout << "Podaj ID pracownika ktoremu chcesz zmienic wynagrodzenie:" <<endl;
+                cin >> new_id;
+                cout << "Podaj nowe wynagrodzenie:" << endl;
+                cin >> new_salary;
+                nowy.changeSalary(new_id,new_salary);
+                break;
+            }
+            case 7:
+            {
+                cout << "Dodawanie nowego pracownika" <<endl;
+                double new_salary;
+                string id,name,surname,depart,position;
+                cout << "Podaj id" <<endl;
+                cin >> id;
+                cout << "Podaj imie" <<endl;
+                cin >> name;
+                cout << "Podaj nazwisko" <<endl;
+                cin >> surname;
+                cout << "Podaj ID departamentu" << endl;
+                cin >> depart;
+                cout << "Podaj pozycje" << endl;
+                cin >> position;
+                cout << "Podaj pensje" << endl;
+                cin >> new_salary;
+                employee add_worker(id, name, surname, depart, position);
+                nowy.add_new_worker(add_worker);
+                nowy.add(add_worker, depart, new_salary);
+                break;
+            }
+            default:
+            {
+                cout << "Wybierz ponownie" <<endl;
+                break;
+            }
+        }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    return 0;
+}
