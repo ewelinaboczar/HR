b547b9be3ded6b000fe0e0673b1dfb00ccec4dd7
ewelinaboczar <ewelina.boczar@student.pk.edu.pl>
2020-12-31 13:48:13 +0100

    add cmake

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Studia/Semestr_III/JIPP/HR/src/HRMS.cpp" "E:/Studia/Semestr_III/JIPP/HR/build/CMakeFiles/test.dir/src/HRMS.cpp.obj"
+  "E:/Studia/Semestr_III/JIPP/HR/src/employee.cpp" "E:/Studia/Semestr_III/JIPP/HR/build/CMakeFiles/test.dir/src/employee.cpp.obj"
+  "E:/Studia/Semestr_III/JIPP/HR/src/main.cpp" "E:/Studia/Semestr_III/JIPP/HR/build/CMakeFiles/test.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
